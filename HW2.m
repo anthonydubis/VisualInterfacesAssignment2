52e186103ccb4d033e068f5229d782dc830a7020
Anthony Dubis <anthonydubis@gmail.com>
2015-03-12 13:24:59 -0400

    Creates normalzied color vectors and does comparisons.

--- HW2.m
+++ HW2.m
@@ -2,5 +2,29 @@
 % Number of segments to divide 255 channel into - bins = segments^3
 num_segments = 6;
 
-img = imread('ppm/i01.ppm');
-hist = getColorHistogram(img, num_segments)
+% Get the image filenames
+imgPath      = 'ppm/';
+imgType      = '*.ppm'; % change based on image type
+imgFiles     = dir([imgPath imgType]);
+hist_vectors = cell(length(imgFiles), 1);
+
+% Create array of normalized histogram vectors for each image
+for i=1:length(imgFiles)
+    % Read the image
+    filename = [imgPath imgFiles(i).name];
+    fprintf(imgFiles(i).name); fprintf('\n');
+    img = imread(filename);
+    
+    % Get the vector with normalized bin values
+    hist3D = getNormalizedColorHistogram(img, num_segments);
+    hist_vectors{i} = reshape(hist3D, [1 num_segments^3]);
+end
+
+comparisons = zeros(length(hist_vectors), 2);
+for i=1:length(hist_vectors)
+    comparisons(i, 1) = i;
+    comparisons(i, 2) = colorCompare(hist_vectors{1}, hist_vectors{i});
+end
+
+[Y, I] = sort(comparisons(:, 2));
+comparisons = comparisons(I, :);
---
+++ getNormalizedColorHistogram.m
@@ -0,0 +1,33 @@
+function [ hist ] = getNormalizedColorHistogram( img, sgmts )
+% getColorHistogram takes in a color image and returns a 3D histogram made
+% up of a number of bins (sgmts^3)
+% img   = the 3D array representing RGB colors at each pixel of an image
+% sgmts = the number of segments each color component (rgb) is broken into
+% hist  = the histogram with pixel counts per bin
+
+hist       = zeros(sgmts, sgmts, sgmts);
+channels   = 255;
+sgmt_len   = channels / sgmts;
+img_size   = size(img);
+sig_pixels = 0;
+
+for i=1:img_size(1)
+    for j=1:img_size(2)
+        % Find rbg segments for pixels and increment their bin in hist
+        % Max the value was 1 to handle cases where pixel value is 0
+        rs = max(ceil(img(i, j, 1) / sgmt_len), 1);
+        gs = max(ceil(img(i, j, 2) / sgmt_len), 1);
+        bs = max(ceil(img(i, j, 3) / sgmt_len), 1);
+        
+        % Ignore pixels that are almost completely black - the "first" bin
+        if rs ~= 1 || gs ~= 1 || bs ~= 1
+            hist(rs, gs, bs) = hist(rs, gs, bs) + 1;
+            sig_pixels = sig_pixels + 1;
+        end
+    end
+end
+
+hist = hist/sig_pixels;
+
+end
+
